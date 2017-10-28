f5ff1f737fcfb481db7debcf3237438988ec261d
Daniel Deutsch <deudan1010@gmail.com>
2017-10-28 12:59:40 +0200

    Update structure and formatting according to eslint

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navbar = () => (
+  <div className="navbar-fixed">
+    <nav>
+      <div className="nav-wrapper grey">
+        <a href="./index.html" className="brand-logo">
+            Developers Rising
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="https://www.meetup.com/DevelopersRising/">Events</a>
+          </li>
+          <li>
+            <a href="./friends.html">Team</a>
+          </li>
+          <li>
+            <a href="./members.html">Members</a>
+          </li>
+          <li>
+            <a href="./legal.html">Legal</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+);
+
+export default Navbar;

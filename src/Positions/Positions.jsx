e6241bbb95c03dbe687963bef590994b1767e609
Daniel Deutsch <deudan1010@gmail.com>
2017-10-28 13:09:07 +0200

    Update Positions component with linting

--- src/Positions/Positions.jsx
+++ src/Positions/Positions.jsx
@@ -3,109 +3,110 @@ import React from 'react';
 const Positions = () => (
   <div className="container">
     <h1 id="positionsintheorganization">Positions in the organization</h1>
-<ol>
-    <li id="managementteam">Management Team</li>
+    <ol>
+      <li id="managementteam">Management Team</li>
 
-    <p>
-      The management team consists of Daniel Deutsch (@DDCreationStudios) and Robert Axelsen
-      (@robeerob), and are in charge of the day to day administration and management. If you have
-      any questions regarding the organization, want to get in contact, or want to report harassment
-      or a complaint, please contact them by posting an issue and tagging them, or send an email to
-      devsrising@gmail.com.
-    </p>
+      <p>
+        The management team consists of Daniel Deutsch (@DDCreationStudios) and Robert Axelsen
+        (@robeerob), and are in charge of the day to day administration and management. If you have
+        any questions regarding the organization, want to get in contact, or want to report
+        harassment or a complaint, please contact them by posting an issue and tagging them, or send
+        an email to devsrising@gmail.com.
+      </p>
 
-    <li id="socialmediamanager">Social Media Manager</li>
+      <li id="socialmediamanager">Social Media Manager</li>
 
-    <p>
-      The social media manager is responsible for managing our social media presence, as well as and
-      responding to any inquiries.
-    </p>
+      <p>
+        The social media manager is responsible for managing our social media presence, as well as
+        and responding to any inquiries.
+      </p>
 
-    <p>
-      You can read more about{' '}
-      <a href="https://github.com/DevelopersRising/DevelopersRising/blob/master/SocialMediaGuidelines.md">
-        our social media guidelines here
-      </a>.
-    </p>
+      <p>
+        You can read more about{' '}
+        <a href="https://github.com/DevelopersRising/DevelopersRising/blob/master/SocialMediaGuidelines.md">
+          our social media guidelines here
+        </a>.
+      </p>
 
-    <p>
-      This position is currently open. Please get in touch with us if you would like to volunteer
-      for this role, by posting an issue and tagging the management team (@DDCreationStudios or
-      @robeerob), or send an email to devsrising@gmail.com.
-    </p>
+      <p>
+        This position is currently open. Please get in touch with us if you would like to volunteer
+        for this role, by posting an issue and tagging the management team (@DDCreationStudios or
+        @robeerob), or send an email to devsrising@gmail.com.
+      </p>
 
-    <li id="marketingmanager">Marketing Manager</li>
+      <li id="marketingmanager">Marketing Manager</li>
 
-    <p>
-      The marketing manager is responsible for both traditional and non-traditional marketing, witch
-      the exception of social media and newsletter and PR.
-    </p>
+      <p>
+        The marketing manager is responsible for both traditional and non-traditional marketing,
+        witch the exception of social media and newsletter and PR.
+      </p>
 
-    <p>
-      This position is currently open. Please get in touch with us if you would like to volunteer
-      for this role, by posting an issue and tagging the management team (@DDCreationStudios or
-      @robeerob), or send an email to devsrising@gmail.com.
-    </p>
+      <p>
+        This position is currently open. Please get in touch with us if you would like to volunteer
+        for this role, by posting an issue and tagging the management team (@DDCreationStudios or
+        @robeerob), or send an email to devsrising@gmail.com.
+      </p>
 
-    <li id="newsletterdirector">Newsletter Director</li>
+      <li id="newsletterdirector">Newsletter Director</li>
 
-    <p>
-      The newsletter manager is responsible for managing, writing and editing our email newsletter.
-    </p>
+      <p>
+        The newsletter manager is responsible for managing, writing and editing our email
+        newsletter.
+      </p>
 
-    <p>
-      This position is currently open. Please get in touch with us if you would like to volunteer
-      for this role, by posting an issue and tagging the management team (@DDCreationStudios or
-      @robeerob), or send an email to devsrising@gmail.com.
-    </p>
+      <p>
+        This position is currently open. Please get in touch with us if you would like to volunteer
+        for this role, by posting an issue and tagging the management team (@DDCreationStudios or
+        @robeerob), or send an email to devsrising@gmail.com.
+      </p>
 
-    <li id="projectmanager">Project Manager</li>
+      <li id="projectmanager">Project Manager</li>
 
-    <p>
-      The project manager is responsible for managing and driving our projects forward to
-      completion.
-    </p>
+      <p>
+        The project manager is responsible for managing and driving our projects forward to
+        completion.
+      </p>
 
-    <p>
-      This position is currently open. Please get in touch with us if you would like to volunteer
-      for this role, by posting an issue and tagging the management team (@DDCreationStudios or
-      @robeerob), or send an email to devsrising@gmail.com.
-    </p>
+      <p>
+        This position is currently open. Please get in touch with us if you would like to volunteer
+        for this role, by posting an issue and tagging the management team (@DDCreationStudios or
+        @robeerob), or send an email to devsrising@gmail.com.
+      </p>
 
-    <li id="publicrelationsmanager">Public Relations Manager</li>
+      <li id="publicrelationsmanager">Public Relations Manager</li>
 
-    <p>
-      The public relations manager is responsible for representing the organization in public
-      matters, as well as covering other aspects of traditional PR.
-    </p>
+      <p>
+        The public relations manager is responsible for representing the organization in public
+        matters, as well as covering other aspects of traditional PR.
+      </p>
 
-    <p>
-      This position is currently open. Please get in touch with us if you would like to volunteer
-      for this role, by posting an issue and tagging the management team (@DDCreationStudios or
-      @robeerob), or send an email to devsrising@gmail.com.
-    </p>
+      <p>
+        This position is currently open. Please get in touch with us if you would like to volunteer
+        for this role, by posting an issue and tagging the management team (@DDCreationStudios or
+        @robeerob), or send an email to devsrising@gmail.com.
+      </p>
 
-    <li id="partnershipdirector">Partnership Director</li>
+      <li id="partnershipdirector">Partnership Director</li>
 
-    <p>
-      The partnership director is responsible for dealing with our partners, such as sponsors,
-      conferences, hackathons, etc. This role also covers the effort of enlisting new partners.
-    </p>
+      <p>
+        The partnership director is responsible for dealing with our partners, such as sponsors,
+        conferences, hackathons, etc. This role also covers the effort of enlisting new partners.
+      </p>
 
-    <p>
-      This position is currently open. Please get in touch with us if you would like to volunteer
-      for this role, by posting an issue and tagging the management team (@DDCreationStudios or
-      @robeerob), or send an email to devsrising@gmail.com.
-    </p>
+      <p>
+        This position is currently open. Please get in touch with us if you would like to volunteer
+        for this role, by posting an issue and tagging the management team (@DDCreationStudios or
+        @robeerob), or send an email to devsrising@gmail.com.
+      </p>
 
-    <li id="otherpositions">Other positions</li>
+      <li id="otherpositions">Other positions</li>
 
-    <p>
-      The positions above are not set in stone. So whether you would like to volunteer to help with
-      something else, or in a role that mixes several areas of responsibility, please get in touch
-      with us by posting an issue and tagging the management team (@DDCreationStudios or @robeerob),
-      or send an email to devsrising@gmail.com.
-    </p>
+      <p>
+        The positions above are not set in stone. So whether you would like to volunteer to help
+        with something else, or in a role that mixes several areas of responsibility, please get in
+        touch with us by posting an issue and tagging the management team (@DDCreationStudios or
+        @robeerob), or send an email to devsrising@gmail.com.
+      </p>
     </ol>
   </div>
 );
